24f4b4b874f23e298578dd094f780805612d7f09
Your NameIryna <Iryna@whatever.com>
2017-07-19 19:15:23 +0200

    first project

---
+++ Control Test 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Test_1
+{
+    enum Cities
+    {
+        
+    }
+
+    struct City
+    {
+        public string NameCity;
+        public int PopulationCity;
+        public int AreaCity;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input cities,their population and area:");
+            string InputCities = Console.ReadLine();
+            
+            //Input cities
+            Console.Write(InputCities);
+            Console.WriteLine("=======================");
+            
+
+            for (int i = 0; i < InputCities.Length; i++)
+            {
+                if (i == '=')
+                {
+                    Console.Write("Name is", InputCities[i]);
+                }
+
+                if (i == ',')
+                {
+                    Console.Write("Population is:", InputCities[i]);
+                }
+                { 
+                if (i == ';') Console.Write("Area is:", InputCities[i]);
+                }
+                Console.Write(InputCities[i]);
+            }
+
+
+            City[] city = new City[]
+            {
+                
+            };
+
+            
+
+            Console.WriteLine("Most populated:");
+            Console.WriteLine("Longest name");
+            Console.WriteLine("Density");
+
+        }
+    }
+}
